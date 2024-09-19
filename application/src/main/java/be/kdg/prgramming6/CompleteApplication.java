b764ce2cf91b6e55e4f7b974792fe0b0b0405940
William Kasasa <william.kasasa@student.kdg.be>
2024-09-19 18:34:06 +0200

    Initial commit

---
+++ application/src/main/java/be/kdg/prgramming6/CompleteApplication.java
@@ -0,0 +1,17 @@
+package be.kdg.prgramming6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+@SpringBootApplication
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern="be.kdg.prog6.*.*Application")
+})
+public class CompleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteApplication.class, args);
+    }
+}
---
+++ common/src/main/java/be/kdg/prgramming6/Main.java
@@ -0,0 +1,7 @@
+package be.kdg.prgramming6;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
